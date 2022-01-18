b82728d91026e035e199782bc4359d37974cacc6
Sergio Valero García <sergiovalero92@gmail.com>
2022-01-18 20:53:53 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import HomePage from './components/home-page/home-page';
+
+function App() {
+  return (<>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
